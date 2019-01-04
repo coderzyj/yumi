14516e68b08ffee22e3e39caa099b4e0379edfbd
yongjie.zhang <yongjie.zhang@ucarinc.com>
2019-01-04 17:06:54 +0800

    搜房网初步搭建，采用jpa，thymeleaf

---
+++ src/main/java/com/lambo/YumiApplication.java
@@ -0,0 +1,14 @@
+package com.lambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumiApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/lambo/config/JpaConfig.java
@@ -0,0 +1,53 @@
+package com.lambo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * 版权声明：CopyRight (c) 2018 ucarinc. All Rights Reserved.
+ *
+ * @author : 张勇杰
+ * @date : 2019/1/3 14:05
+ * @Version : v1.0
+ * @description
+ **/
+@Configuration
+@EnableJpaRepositories(basePackages = "com.lambo.repository")
+@EnableTransactionManagement
+public class JpaConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        HibernateJpaVendorAdapter jpaVendor = new HibernateJpaVendorAdapter();
+        jpaVendor.setGenerateDdl(false);
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setJpaVendorAdapter(jpaVendor);
+        entityManagerFactory.setPackagesToScan("com.lambo.entity");
+        return entityManagerFactory;
+    }
+
+    @Bean
+    PlatformTransactionManager transactionManager(EntityManagerFactory
+                                                  entityManagerFactory){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
